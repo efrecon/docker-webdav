e86095e04166c7574557342ca2378941bdbcaef0
Emmanuel Frecon <emmanuel.frecon@myjoice.com>
2016-06-20 14:35:50 +0200

    Making sure we can lock using an sqlite database

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,10 @@ fi
 
 chown webdav /var/log/lighttpd
 
+# Create directory to hold locks
+mkdir /locks
+chown ${USERNAME}:${GROUP} /locks
+
 # Force the /webdav directory to be owned by webdav/webdav otherwise we won't be
 # able to write to it. This is ok if you mount from volumes, perhaps less if you
 # mount from the host, so do this conditionally.
